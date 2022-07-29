f41e0804dedb554a9667bdb642d9caed6620d2bb
= <=>
2022-07-29 10:16:44 -0600

    checkpoint1

--- sdlc.js
+++ sdlc.js
@@ -4,7 +4,12 @@
 /*
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
-
+console.log('Planning')
+console.log('Analysis of requirements')
+console.log('Design')
+console.log('Testing and integration')
+console.log('Implementaion')
+console.log('Maintain')
 // Implementation
 // Design
 // Maintain
@@ -18,4 +23,15 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisAndRequirements = " This is the second step, in this step the analysis of different ideas begin as well as any requirements needed."
+const design = "This is the third step in the SLDC process, developers will outline the details for the overall project, in this stage they also begin designing 'user interfaces, system interfaces, databases etc."
+const testingAndIntegration = "This is the fourth step, after designing developers will test said design to find any bugs, and issues with the code, as well as its effiency."
+const Implementaion = "This is the fifth step, after the final design of the software, differnt modules will be added to the primary source code. They do a final test to find any minor errors they might have missed, once doen it is technically ready for market."
+const maintain = "This is the last step of the SLDC process, developers will move into the maintenance stage when the software reaches the market, developers will practice any activities required to handle issues reported by customers. After this stage is complete it is ready to be downloaded by end-users"
+
+
+
+
+
+
