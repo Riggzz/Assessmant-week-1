688cb714b7e94f58069688b3efb4f0170c3d86f0
= <=>
2022-07-29 12:42:17 -0600

    final

---
+++ password.js
@@ -0,0 +1,4 @@
+const passWord = `SPEEDRAC3R`  
+if (passWord.length >= 10 && passWord.includes(Number(3)) || passWord.includes(Number(1)) || passWord.includes(Number(2)) || passWord.includes(Number(4)) || passWord.includes(Number(5)) || passWord.includes(Number(6)) || passWord.includes(Number(7)) || passWord.includes(Number(8)) || passWord.includes(Number(9)) || passWord.includes(Number(0))){
+    console.log('Success!')}
+    else (console.log('Failure'));
